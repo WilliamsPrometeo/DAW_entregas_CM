1d81b68e4469f841c1fed6350a1ce9be43481d0f
GermanVillar <german.villar.ramos@edu.thepower.education>
2025-11-19 13:11:17 +0100

    Entrega de ejercicio.

---
+++ GermanVillar/src/Entrega2/src/MyScanner.java
@@ -0,0 +1,17 @@
+package Entrega2.src;
+
+import java.util.Scanner;
+
+class MyScanner {
+
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static double nextDouble() {
+        return scanner.nextDouble();
+    }
+
+    public static String nextLine() {
+        return scanner.nextLine();
+    }
+}
+
