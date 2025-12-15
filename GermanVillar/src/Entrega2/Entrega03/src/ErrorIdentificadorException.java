5d7e1a02fb11c60d6be384d28c09b691baa0bde5
GermanVillar <german.villar.ramos@edu.thepower.education>
2025-12-15 12:55:28 +0100

    Entrega03

---
+++ GermanVillar/src/Entrega2/Entrega03/src/ErrorIdentificadorException.java
@@ -0,0 +1,7 @@
+package Entrega2.Entrega03.src;
+
+public class ErrorIdentificadorException extends Exception {
+    public ErrorIdentificadorException(String message) {
+        super(message);
+    }
+}
